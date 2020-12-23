901551bac55220e7e458fb7e6069884808aad2e6
kairedchan@qq.com <Chengkai11011>
2020-12-23 11:10:36 +0800

    测试学习使用

--- src/main/java/com/greatmap/demo/DemoApplication.java
+++ src/main/java/com/greatmap/demo/DemoApplication.java
@@ -1,13 +1,41 @@
 package com.greatmap.demo;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.ResourceBanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.PrintStream;
+import java.net.InetAddress;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		try {
+//			SpringApplicationBuilder springApplicationBuilder = new SpringApplicationBuilder(DemoApplication.class);
+//			ConfigurableApplicationContext context = springApplicationBuilder.headless(false).run(args);
+//
+//			Environment env = context.getEnvironment();
+//			String host = InetAddress.getLocalHost().getHostAddress();
+//			String port = env.getProperty("server.port");
+//			String contextPath = env.getProperty("server.servlet.context-path");
+//			contextPath = contextPath == null ? "" : contextPath;
+//			System.out.println("项目启动完成,地址：http://" + host + ":" + port + contextPath + "/");
+//			System.out.println("Swagger2接口文档地址：http://" + host + ":" + port + contextPath + "/swagger-ui.html");
+
+			SpringApplication bootstrap = new SpringApplication(DemoApplication.class);
+			bootstrap.setBanner(new ResourceBanner(new ClassPathResource("banner.txt")));
+			bootstrap.setBannerMode(Banner.Mode.CONSOLE);
+			bootstrap.run(args);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
